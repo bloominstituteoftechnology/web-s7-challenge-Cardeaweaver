ee68ca291d578483600521c10e719108233783b2
carde'a weaver <165233514+Cardeaweaver@users.noreply.github.com>
2024-06-17 17:51:40 -0400

    sprint 1

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <nav>
         {/* NavLinks here */}
       </nav>
+        <Routes>
+          <Route>
+            
+             </Route>
+        </Routes>
+
       {/* Route and Routes here */}
       <Home />
       <Form />
